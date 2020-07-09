8dbabec6a0db1f81e310107a425a5232cb6a56d3
Aramiskkck69 <jlaramis9@hotmail.com>
2020-07-08 22:31:18 -0600

    Maquetado Alpha de About

---
+++ project-angular/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { ModuleWithProviders } from "@angular/core"; // Crear modulos
+import { Routes, RouterModule } from "@angular/router"; // creacion de rutas
+
+
+import { AboutComponent } from './components/about/about.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ErrorComponent } from './components/error/error.component';
+
+//************************ Rutas de las paginas web ****************************************
+const  appRoutes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'sobre-mi', component: AboutComponent},
+  {path: 'proyectos', component: ProjectsComponent},
+  {path: 'crear-proyecto', component: CreateComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: '***', component: ErrorComponent},
+];
+//**************************Exportar configuracion de las rutas ****************************************
+export const appRoutingProviders: any[] = []; // exportar el servicio de rutas
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes); // modulo que carga la configuracion de rutas
---
+++ project-angular/src/app/components/about/about.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle:string;
+  public email:string;
+  constructor() {
+    this.title = "J.L Aramis Ramirez";
+    this.subtitle = 'Ing. Mecatronico';
+    this.email = 'joluramirezdi@ittepic.edu.mx';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
