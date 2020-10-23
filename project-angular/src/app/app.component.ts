02df69b12370493d4c8935fd3a5f9d8e80927a78
jlara <jlaramis9@hotmail.com>
2020-10-22 18:36:34 -0600

    Maquetado de inicio de sesion

--- project-angular/src/app/app.component.ts
+++ project-angular/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  public sesion:boolean;
+  public contador:number;
+  constructor() {
+    this.sesion = false;
+    this.contador = 0;
+  }
+  ngOnInit(): void{
+  }
+
   title = 'project-angular';
+
+  sesionActive(){
+    this.sesion = true;
+    this.contador = this.contador + 1;
+    if (this.contador > 1){
+      this.sesion = false;
+      this.contador=0;
+      console.log(this.contador);
+    }
+  }
+  sesionDesactive(){
+    this.sesion = false;
+    this.contador = 0;
+  }
+
 }
+
--- project-angular/src/app/components/about/about.component.ts
+++ project-angular/src/app/components/about/about.component.ts
@@ -9,10 +9,12 @@ export class AboutComponent implements OnInit {
   public title: string;
   public subtitle:string;
   public email:string;
+  public sesion: boolean;
   constructor() {
     this.title = "J.L Aramis Ramirez";
     this.subtitle = 'Ing. Mecatronico';
     this.email = 'joluramirezdi@ittepic.edu.mx';
+    this.sesion = false;
   }
 
   ngOnInit(): void {
